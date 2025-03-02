870704db6914edbb781282679f30818f1c4deb3c
Snehit Paunikar <snehit.86@gmail.com>
2025-03-03 01:51:23 +0530

    removed secrets

--- cmd/main.go
+++ cmd/main.go
@@ -40,7 +40,6 @@ func main() {
 
 func setUpDatabase() *gorm.DB {
 	dsn := fmt.Sprintf("user=%s password=%s dbname=%s port=%s sslmode=%s", envConfig.DbUsername, envConfig.DbPassword, envConfig.DbName, envConfig.DbPort, envConfig.DbSSLMode)
-	//dsn := "postgres://avnadmin:AVNS_PuwYbVmsPbBzKk83CMH@books-management-system-books-management-system.l.aivencloud.com:28842/defaultdb?sslmode=require"
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN:                  dsn,
 		PreferSimpleProtocol: true,
@@ -57,9 +56,6 @@ func setUpKafkaProducer() *kafka.KafkaProducer {
 
 func setUpRedis() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		// Addr:     "redis-18205.c72.eu-west-1-2.ec2.redns.redis-cloud.com:18205",
-		// Password: "kw7ho6G8OFw1EkLSScyAzToZA6xMdqfc",
-		// DB:       0,
 		Addr:     envConfig.RedisAddress,  // e.g. "localhost:6379"
 		Password: envConfig.RedisPassword, // no password set
 		DB:       envConfig.RedisDB,       // use default DB
